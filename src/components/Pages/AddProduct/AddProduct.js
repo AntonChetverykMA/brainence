536da697f654f5440ee6a4a97b678ad953737718
AntonChetveryk <chetverykanton92@gmail.com>
2020-11-08 12:32:54 +0200

    fixed form position

--- src/components/Pages/AddProduct/AddProduct.js
+++ src/components/Pages/AddProduct/AddProduct.js
@@ -12,59 +12,61 @@ function AddProduct(props) {
   let history = useHistory();
 
   return (
-    <form>
-      <div className='form-group'>
-        <label htmlFor='name'>Name of product</label>
-        <input
-          type='text'
-          className='form-control mb-4'
-          id='name'
-          placeholder='name'
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <label htmlFor='description'>Description</label>
-        <input
-          type='text'
-          className='form-control mb-4'
-          id='description'
-          placeholder='description'
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-        />
-        <label htmlFor='img'>Add link to img</label>
-        <input
-          type='text'
-          className='form-control mb-4'
-          id='img'
-          placeholder='img'
-          value={img}
-          onChange={(e) => setImg(e.target.value)}
-        />
-        <label htmlFor='price'>Price</label>
-        <input
-          type='number'
-          className='form-control'
-          id='price'
-          placeholder='price'
-          value={price}
-          onChange={(e) => setPrice(e.target.value)}
-        />
-      </div>
+    <div className='d-flex justify-content-center'>
+      <form>
+        <div className='form-group'>
+          <label htmlFor='name'>Name of product</label>
+          <input
+            type='text'
+            className='form-control mb-4'
+            id='name'
+            placeholder='name'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <label htmlFor='description'>Description</label>
+          <input
+            type='text'
+            className='form-control mb-4'
+            id='description'
+            placeholder='description'
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+          <label htmlFor='img'>Add link to img</label>
+          <input
+            type='text'
+            className='form-control mb-4'
+            id='img'
+            placeholder='img'
+            value={img}
+            onChange={(e) => setImg(e.target.value)}
+          />
+          <label htmlFor='price'>Price</label>
+          <input
+            type='number'
+            className='form-control'
+            id='price'
+            placeholder='price'
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+          />
+        </div>
 
-      <button
-        type='button'
-        className='btn add'
-        onClick={() => {
-          if ((name, description, img, price)) {
-            props.addProduct({ name, description, img, id: uuidv4(), price });
-            history.push('/');
-          } else alert('Fill in all the gaps');
-        }}
-      >
-        Add
-      </button>
-    </form>
+        <button
+          type='button'
+          className='btn add'
+          onClick={() => {
+            if ((name, description, img, price)) {
+              props.addProduct({ name, description, img, id: uuidv4(), price });
+              history.push('/');
+            } else alert('Fill in all the gaps');
+          }}
+        >
+          Add
+        </button>
+      </form>
+    </div>
   );
 }
 
